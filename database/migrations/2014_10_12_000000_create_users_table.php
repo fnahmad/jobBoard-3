42d79e88da6541c29a60dff9004bb2848e86ab1b
EtienneFontaine <etienne.fontaine1@gmail.com>
2018-05-04 13:55:31 +0200

    feat(users): add register and login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,32 +4,35 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+class CreateUsersTable extends Migration{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('users', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('firstname');
+			$table->string('lastname');
+			$table->string('promo');
+			$table->string('phone')->nullable();
+			$table->string('email')->unique();
+			$table->string('email_token');
+			$table->boolean('is_admin')->default(false);
+			$table->boolean('is_confirmed')->default(false);
+			$table->string('password');
+			$table->rememberToken();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('users');
+	}
 }
---
+++ resources/views/email/confirm-account.blade.php
@@ -0,0 +1,2 @@
+<h1>Cliquer sur le lien pour confirmer votre compte</h1>
+<p><a href="{{url('/confirm-account/'.$email_token)}}" target="_blank">Confirmer mon compte</a></p>
