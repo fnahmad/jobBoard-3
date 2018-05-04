72d7365621adb6d82ac6dbab14426ec685427ed2
EtienneFontaine <etienne.fontaine1@gmail.com>
2018-05-04 20:33:00 +0200

    add offer add

--- resources/assets/js/components/SearchHeader.vue
+++ resources/assets/js/components/SearchHeader.vue
@@ -1,52 +1,57 @@
 <template>
     <header class="search-header">
-        <h1 class="search-header__title"><strong>120</strong> offres</h1>
-        <ais-input placeholder="laravel, vue, react, ..."></ais-input>
+        <h1 class="search-header__title">
+            <ais-stats>
+                <template slot-scope="{ totalResults }">
+                    <strong>{{ totalResults }}</strong>{{ totalResults > 1 ? ' offres' : ' offre' }}</template>
+            </ais-stats>
+        </h1>
+        <ais-input placeholder="Rechercher une techno"></ais-input>
     </header>
 </template>
 
 <script>
 
-export default {
-  
-}
+	export default {
+
+	}
 </script>
 
 <style lang="scss">
-.search-header{
-  width: 100%;
-  height: 300px;
-  background: linear-gradient(to right, #da4453, #89216b);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  &__title{
-    font-size: 3.6rem;
-    font-weight: 300;
-    font-family: var(--main-font);
-    color: var(--white);
-    strong{
-      font-weight: 600;    
-    }
-  }
+    .search-header{
+        width: 100%;
+        height: 300px;
+        background: linear-gradient(to right, #da4453, #89216b);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        &__title{
+            font-size: 3.6rem;
+            font-weight: 300;
+            font-family: var(--main-font);
+            color: var(--white);
+            strong{
+                font-weight: 600;
+            }
+        }
 
-  .ais-input{
-    width: 30vw;
-    padding: 10px 30px;
-    background: var(--white);
-    border: none;
-    border-radius: 50px;
-    outline: none;
-    font-size: 1.8rem;
-    color: var(--black);
-    font-family: var(--main-font);
-    &::placeholder{
-      color: rgba(#000, .3);
+        .ais-input{
+            width: 30vw;
+            padding: 10px 30px;
+            background: var(--white);
+            border: none;
+            border-radius: 50px;
+            outline: none;
+            font-size: 1.8rem;
+            color: var(--black);
+            font-family: var(--main-font);
+            &::placeholder{
+                color: rgba(#000, .3);
 
+            }
+        }
     }
-  }
-}
 
 </style>
 
---
+++ resources/assets/js/components/SelectSkills.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="form-group">
+        <label>Compétances</label>
+        <v-select multiple v-model="selected" :options="options" :closeOnSelect="false"></v-select>
+        <input type="text" v-model="selected" name="skills[]" hidden>
+    </div>
+</template>
+
+<script>
+	import vSelect from 'vue-select'
+
+	export default {
+		name: "SelectSkills",
+		props: ['options'],
+		components: {
+			vSelect
+		},
+		data() {
+			return {
+				selected: []
+			}
+		}
+	}
+</script>
