2071f36e8a1fa112817262579ea8780abbcd0ea6
Antonin Carlin <antonin@221.rocks>
2018-05-05 19:50:29 +0200

    feat(admin): layout

---
+++ resources/assets/js/layouts/helper.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <component :is="as">
+      <slot />
+    </component>
+  </section>
+</template>
+
+<script>
+import backoffice from './backoffice';
+import principle from './principle';
+import _omit from 'lodash/omit'
+export default {
+  name: 'layout',
+  props: {
+    as: {
+      type: String,
+      default: 'principle'
+    }
+  },
+   components: { backoffice, principle }
+}
+</script>
