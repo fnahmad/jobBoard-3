544b8036507a70a1f5d03708f69430df7485e091
bvillain92@gmail.com <BVillain92@gmail.com>
2018-05-05 12:27:17 +0200

    :lipstick: card design

---
+++ resources/assets/js/components/ResultPeopleCount.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="result-people">
+    {{ count }}
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/></svg>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['count']
+}
+</script>
+
+<style lang="scss">
+.result-people{
+  margin-left: 15px;
+  display: inline-block;
+  font-size: 1.8rem;
+  font-weight: 600;
+  color: var(--light-grey);
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  svg {
+    margin-left: 5px;
+    width:  16px;
+    height: 16px;
+    fill: var(--light-grey);
+  }
+}
+</style>
