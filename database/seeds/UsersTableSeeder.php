b43bbd7d94b56cf0734dc4a9c75cece3644f41cf
EtienneFontaine <etienne.fontaine1@gmail.com>
2018-05-05 11:41:05 +0200

    fix(docker): remove mobule

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		User::where('email', 'jobboard@example.com')->delete();
+
+		$user               = new User();
+		$user->firstname    = 'Job';
+		$user->lastname     = 'Board';
+		$user->email        = 'jobboard@example.com';
+		$user->email_token  = base64_encode('jobboard@example.com');
+		$user->password     = base64_encode('jobBoardHetic');
+		$user->promo        = '2020';
+		$user->is_confirmed = 1;
+		$user->save();
+	}
+}
