a69da719e602465af2139a4eb53c9dcc7cee1237
Antonin Carlin <antonin@221.rocks>
2018-05-05 20:42:45 +0200

    fix(moment): remove local and check case sensitive

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,32 +1,63 @@
 
-/**
+/***
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
- */
+ *  
+***/
 
 require('./bootstrap');
 
 import 'normalize.css'
 
-window.Vue = require('vue');
-
-import InstantSearch from 'vue-instantsearch';
-import VueClipboard from 'vue-clipboard2'
 window.moment = require('moment')
 window.moment.locale('fr')
-window.Vue.use(InstantSearch);
-window.Vue.use(VueClipboard)
+const mapToFunc = (array, func, args) => {
+  array.forEach(element => {
+    func(element, ...args)
+  });
+}
 
+import 'normalize.css'
 import Vue from 'vue'
+
+// ---- IMPORT GLOBAL COMPONENTS ---- //
+import VueClipboard from 'vue-clipboard2'
 import vSelect from './components/SelectSkills.vue'
+import LayoutHelper from './layouts/helper.vue'
 
-Vue.component('v-select', vSelect);
+// ---- IMPORT PAGES ---- //
+import Root from './components/App.vue'
 
+const globalComponents = [
+    // components
+    vSelect, LayoutHelper, VueClipboard,
+    // pages
+    Root
+]
 
+// ---- IMPORT GLOBAL PLUGINS ---- //
+// import InstantSearch from 'vue-instantsearch';
 
-Vue.component('app', require('./components/app.vue'));
+const plugins = [InstantSearch]
+// Autoload components
+mapToFunc(globalComponents, (component, vue) => {
+  if (component && component.name && vue) {
+    vue.component(component.name, component)
+  } else {
+    throw `check if your component has a name`
+   }
+}, [Vue])
+
+// Autoload plugins
+// mapToFunc(plugins,  (plugin, vue) => {
+//    if (plugin && vue) {
+//        vue.use(plugin)
+//    } else {
+//      throw `check if your import your plugin correctly`
+//    }
+// }, [Vue])
 
 const app = new Vue({
-    el: '#app',
+    el: '#app'
 });
