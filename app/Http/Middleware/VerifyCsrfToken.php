fad855efdf52d0ec77e8fab5c84cd6b0e651f1d0
EtienneFontaine <etienne.fontaine1@gmail.com>
2018-05-13 17:48:32 +0200

    fix(logout): remove csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,15 +3,20 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Session\TokenMismatchException;
 
-class VerifyCsrfToken extends Middleware
-{
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array
-     */
-    protected $except = [
-        //
-    ];
+class VerifyCsrfToken extends Middleware{
+	protected $except_urls = [
+		'logout',
+	];
+
+	public function handle($request, \Closure $next) {
+		$regex = '#' . implode('|', $this->except_urls) . '#';
+
+		if($this->isReading($request) || $this->tokensMatch($request) || preg_match($regex, $request->path())) {
+			return $this->addCookieToResponse($request, $next($request));
+		}
+
+		throw new TokenMismatchException();
+	}
 }
